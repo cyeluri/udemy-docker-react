8adca02c095c04360ccd6c6c9a3ac1be4eb98fb1
cyeluri <chaitanya.yeluri@vertiv.com>
2020-08-17 08:45:30 -0400

    updated App.js file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React App with Travis-CI
+          React App with Travis-CI Dev Branch
         </p>
         <a
           className="App-link"
